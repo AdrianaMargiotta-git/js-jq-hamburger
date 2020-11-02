b76bc03a2a77ddf14b2ac34acfca97a3ca0e2134
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2020-11-02 18:31:53 +0100

    soluzioni alternative

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //mostrare / nascondere il menu principale
 
 //attenzione al tag i
+//primo metodo
 $(".fa-bars").click(
   function(){
     $(".hamburger-menu").fadeIn();
@@ -12,3 +13,23 @@ $(".fa-times").click(
     $(".hamburger-menu").fadeOut();
   }
 );
+
+//secondo metodo
+/*$(".fa-bars").click(function () {
+  $(".hamburger-menu").addClass('active');
+});
+
+$(".fa-times").click(function () {
+  $(".hamburger-menu").removeClass('active');
+});*/
+
+//terzo metodo
+/*$('.header-right > a').click(function() {
+  $('.hamburger-menu').addClass('active');
+  }
+);
+
+$('.hamburger-menu > a').click(function() {
+  $('.hamburger-menu').removeClass('active');
+  }
+);*/
